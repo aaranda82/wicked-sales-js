40577fea3971fcebda51f93786606c93afb351bc
Alex Aranda <alexraranda@gmail.com>
2020-01-14 12:33:15 -0800

    view products front end completed using mostly bootstrap to arrange products, formated price to show as dollar value

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function formatPrice(price) {
+  price = ('$' + price);
+  price = price.split('');
+  price.splice(price.length - 2, 0, '.');
+  price = price.join('');
+  return price;
+}
+
+function ProductListItem(props) {
+  const product = props.product;
+  const price = formatPrice(product.price);
+  return (
+    <div className="card col-3 m-3">
+      <img src= {product.image} alt={product.name} className="card-image-top" height="150vh" width="auto"/>
+      <div className="card-body">
+        <h5 className="card-title">{product.name}</h5>
+        <div>{price}</div>
+        <p className="card-text">{product.shortDescription}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductListItem;
---
+++ client/components/product-list.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(products => this.setState({ products: products }))
+      .catch(err => console.error(err));
+  }
+
+  generateItems() {
+    if (this.state.products.length > 0) {
+      const productArray = this.state.products.map(index => {
+        return <ProductListItem product={index} key={index.productId} />;
+      });
+      return productArray;
+    }
+  }
+
+  render() {
+    return (
+      <div className="conatiner bg-light">
+        <div className="row justify-content-center">
+          {this.generateItems()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
