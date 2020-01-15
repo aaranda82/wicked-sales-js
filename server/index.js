b9c8a34338ab56b34670acad6e7438837889108a
Alex Aranda <alexraranda@gmail.com>
2020-01-15 10:48:54 -0800

    add to cart back end complete without comments

--- server/index.js
+++ server/index.js
@@ -46,7 +46,6 @@ app.get('/api/products/:productId', (req, res, next) => {
     })
     .catch(err => next(err));
 });
-/// ////////////////////////////////////////////
 
 app.get('/api/cart', (req, res, next) => {
   if (!req.session.cartId) {
@@ -126,7 +125,6 @@ app.post('/api/cart', (req, res, next) => {
     .catch(err => next(err));
 });
 
-/// //////////////////////////////////////////
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
