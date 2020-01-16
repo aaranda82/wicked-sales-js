b2f04ec74f1b2608402342d5329b114238cd713a
Alex Aranda <alexraranda@gmail.com>
2020-01-15 17:55:15 -0800

    view cart summary front end complete

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function CartSummaryItem(props) {
+  const product = props.product;
+  let price = (product.price / 100).toFixed(2);
+  price = `$${price}`;
+  return (
+    <div className="card mb-3">
+      <div className="row">
+        <div className="col-4">
+          <img src={product.image} alt={product.name} className="mx-auto d-block" height="150vh"/>
+        </div>
+        <div className="col-6">
+          <h5>{product.name}</h5>
+          <div>{price}</div>
+          <div>{product.shortDescription}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartSummaryItem;
---
+++ client/components/cart-summary.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CartSummaryItem from './cart-summary-item';
+
+function CartSummary(props) {
+  const cartItems = props.cartItems;
+  if (cartItems.length === 0) {
+    return <h5>Cart Empty</h5>;
+  } else {
+    const cartItemArr = cartItems.map(index => <CartSummaryItem product={index} key={index.productId}/>);
+    let cartTotal = cartItems.reduce((acc, cur) => acc + cur.price, 0);
+    cartTotal = (cartTotal / 100).toFixed(2);
+    cartTotal = `$${cartTotal}`;
+    return (
+      <React.Fragment>
+        <div className="backbutton col-12" onClick={() => props.setView('catalog', {})}>{'<Back to catalog'}</div>
+        <h1>My Cart</h1>
+        <div className="container">
+          {cartItemArr}
+        </div>
+        <h3>{`Cart Total: ${cartTotal}`}</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CartSummary;
