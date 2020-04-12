bbd1f660b1e5e572b29cb79097b87a609f5ed0ee
Alex Aranda <alexraranda@gmail.com>
2020-04-12 09:53:30 -0700

    added styled-components package, and defined global color scheme

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ProductListItem from './product-list-item';
+import { ColorScheme } from '../ColorScheme';
+import styled from 'styled-components';
 
 class ProductList extends React.Component {
   constructor(props) {
@@ -21,19 +23,25 @@ class ProductList extends React.Component {
   generateItems() {
     if (this.state.products.length > 0) {
       const productArray = this.state.products.map(index => {
-        return <ProductListItem product={index} key={index.productId} setView={this.props.setView}/>;
+        return (
+          <ProductListItem
+            product={index}
+            key={index.productId}
+            setView={this.props.setView}
+          />
+        );
       });
       return productArray;
     }
   }
 
   render() {
+    const Container = styled.div`
+    background-color: ${ColorScheme.black}`;
     return (
-      <div className="conatiner bg-light">
-        <div className="row justify-content-center">
-          {this.generateItems()}
-        </div>
-      </div>
+      <Container className="conatiner">
+        <div className="row justify-content-center">{this.generateItems()}</div>
+      </Container>
     );
   }
 }
